7d09c83ec44b47ba3864ec91fe7162c0daf55b29
jamie2779 <jamie2779@naver.com>
2023-09-25 17:42:24 +0900

    feat: Add OTP generate process

--- api/app.py
+++ api/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, abort, session
 from flask_cors import CORS
 import DB
 import os
+import random
+import time
 
 #Flask 앱 생성 및 설정
 app = Flask(__name__)
@@ -13,6 +15,9 @@ CORS(app, resources={r'*': {'origins': '*'}})
 #배포 주소 및 포트
 host_addr = "0.0.0.0"
 host_port = 5000
+
+#otp 작업 저장해둘 딕셔너리, key = otp, value = info
+otp_list = {}
 
 #응답 형식 반환
 def response_format(msg,data={}):
@@ -44,11 +49,30 @@ def login():
         abort(400)
 
 #로그아웃 기능
-@app.route("/api/auth/logout",methods=['GET'])
+@app.route("/api/auth/logout",methods=['POST'])
 def logout(): 
     if "username" in session: 
         session.clear()
         return response_format("Success")
+    else:
+        return abort(401)
+
+@app.route("/api/auth/otp/gen",methods=['GET'])
+def get_otp(): 
+    if "username" in session: 
+        body = request.get_json()
+        if "type" in body and "data" in body:
+            type = body["type"]
+            data = body["data"]
+            while True:
+                otp = random.randint(0,9999)
+                if otp not in otp_list:
+                    break
+            otp_info = {"expires":time.time()+180,"type":type,"data":data}
+            otp_list[otp] = otp_info
+            return response_format("Success",{"otp":otp})
+        else:
+            return abort(400)
     else:
         return abort(401)
 
